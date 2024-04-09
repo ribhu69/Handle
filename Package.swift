1d6c4a4e746aec180990323b9e3bdb1c27af309f
Arkaprava Ghosh <arkapravaghosh99@gmail.com>
2024-04-09 12:47:13 +0530

    Add platform target for library

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Handle",
+    platforms: [
+        .iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
